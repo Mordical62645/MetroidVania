9e88aa53f01b0f18ef792e918cc1afb638c008d8
Mordical62645 <85152272+Mordical62645@users.noreply.github.com>
2021-06-07 17:57:19 -0700

    Update Player Jump
    Jumping now have the ability to constantly rise (eventually be grounded) when the button is active. Changing the duration depending on the active duration of the button.

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -7,28 +7,59 @@ public class PlayerJump : MonoBehaviour
    //Force, apply force, 1x
    //rb.velocity = new Vector2(rb.velocity.x, jumpforce);
 
-   [Header("Public Vars")]
+   [Header("Jump Details")]
    public float jumpForce;
-   public bool grounded;
-   private Rigidbody2D rb;
-   
-   [Header("Private Vars")]
+   public float jumpTime;
+   private float jumpTimeCounter;
+   private bool stoppedJumping;
+
+   [Header("Ground Details")]
    [SerializeField] private Transform groundCheck;
    [SerializeField] private float radOCircle;
    [SerializeField] private LayerMask whatIsGround;
+   public bool grounded;
 
+   [Header("Components")]
+   private Rigidbody2D rb;
+   
+   
    private void Start()
    {
        rb = GetComponent<Rigidbody2D>();
+       jumpTimeCounter = jumpTime;
    }
 
    private void Update()
    {
+       //Basically grounded 
        grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround);
+       
+       if (grounded)
+       {
+           jumpTimeCounter = jumpTime;
+       }
+
        //Use w or space to jump
        if(Input.GetButtonDown("Jump") && grounded)
        {
+           //Jump!!!
            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+           stoppedJumping = false;
+       }
+
+       //To keep jumping while button is active
+       if(Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0))
+       {
+           //Jump!!
+           rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+           jumpTimeCounter -= Time.deltaTime;
+       }
+
+       //When jump button is inactive (released)
+       if (Input.GetButtonUp("Jump"))
+       {
+           jumpTimeCounter = 0;
+           stoppedJumping = true;
        }
    }
    private void OnDrawGizmos()
